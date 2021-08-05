9eb2a172c8590f6b4810be79dd9d2d86d6a8029b
Clzzi <clzzi1109@naver.com>
2021-08-06 05:34:00 +0900

    Add: main page data fetching

--- covid-web-next/pages/index.tsx
+++ covid-web-next/pages/index.tsx
@@ -1,16 +1,63 @@
 import Button from "components/Button";
 import Card from "components/Card";
 import Title from "components/Title";
+import { getAllCondition } from "lib/apis";
 import cx from "styles/Main.module.scss";
 
-const Main = () => {
+interface GetAllCondition {
+  resultCode: string;
+  TotalCase: string;
+  TotalRecovered: string;
+  TotalDeath: string;
+  NowCase: string;
+  city1n: string;
+  city2n: string;
+  city3n: string;
+  city4n: string;
+  city5n: string;
+  city1p: string;
+  city2p: string;
+  city3p: string;
+  city4p: string;
+  city5p: string;
+  recoveredPercentage: number;
+  deathPercentage: number;
+  checkingCounter: string;
+  checkingPercentage: string;
+  caseCount: string;
+  casePercentage: string;
+  notcaseCount: string;
+  notcasePercentage: string;
+  TotalChecking: string;
+  TodayRecovered: string;
+  TodayDeath: string;
+  TotalCaseBefore: string;
+  source: string;
+  updateTime: string;
+  resultMessage: string;
+}
+
+const Main = ({ data }: { data: GetAllCondition }) => {
+  console.log(data);
   return (
     <div className={cx.Main}>
-      <Title content="205, 702 명" number="- 61" />
+      <Title
+        content={`${data.TotalCase} 명`}
+        number={`+ ${data.TotalCaseBefore}`}
+      />
       <Card title="완치자" subTitle="+ 1596" content="180,719 명" />
-      <Button content="대구" active={false} />
+      {/* <Button content="대구" active={false} /> */}
     </div>
   );
 };
 
 export default Main;
+
+export const getServerSideProps = async () => {
+  const res = await getAllCondition();
+  return {
+    props: {
+      data: res,
+    },
+  };
+};
