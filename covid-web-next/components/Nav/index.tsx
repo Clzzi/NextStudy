d673b54c84e72af6bf7ec6678a47e7b0e3872f04
Clzzi <clzzi1109@naver.com>
2021-08-06 04:11:44 +0900

    Add: Nav Bar

--- covid-web-next/components/Nav/index.tsx
+++ covid-web-next/components/Nav/index.tsx
@@ -1,11 +1,37 @@
 import { FC } from "react";
-import classNames from "classnames";
-import styles from 'styles/Nav.module.scss';
+import cx from "styles/Nav.module.scss";
+import Link from "next/Link";
 
 const Nav: FC = ({ children }) => {
   return (
-    <div className={styles.Nav}>
-      nav
+    <div className={cx.Wrap}>
+      <div className={cx.Nav}>
+        <div className={cx.Top}>
+          <div className={cx.Title}>COVID</div>
+        </div>
+        <div className={cx.Content}>
+          <div className={cx.ContentTopTitle}>8.5.00시 동기화</div>
+          <ul className={cx.ContentLists}>
+            <li className={cx.ContentList}>
+              <Link href="/">
+                <a>국내 총 현황</a>
+              </Link>
+            </li>
+            <li className={cx.ContentList}>
+              <Link href="/">
+                <a>국내 시도별 현황</a>
+              </Link>
+            </li>
+          </ul>
+          <div className={cx.Bottom}>
+            Created By{" "}
+            <strong>
+              <Link href="https://github.com/Clzzi">@Clzzi</Link>
+            </strong>
+          </div>
+        </div>
+      </div>
+      {children}
     </div>
   );
 };
