31303829051825bd92902b5d8caf21d567c14a4e
Clzzi <clzzi1109@naver.com>
2021-08-06 07:43:45 +0900

    Add: types

---
+++ covid-web-next/lib/types/index.ts
@@ -0,0 +1,32 @@
+export interface GetAllCondition {
+  resultCode: string;
+  TotalCase: string;
+  TotalRecovered: string;
+  TotalDeath: string;
+  NowCase: string;
+  city1n: string;
+  city2n: string;
+  city3n: string;
+  city4n: string;
+  city5n: string;
+  city1p: string;
+  city2p: string;
+  city3p: string;
+  city4p: string;
+  city5p: string;
+  recoveredPercentage: number;
+  deathPercentage: number;
+  checkingCounter: string;
+  checkingPercentage: string;
+  caseCount: string;
+  casePercentage: string;
+  notcaseCount: string;
+  notcasePercentage: string;
+  TotalChecking: string;
+  TodayRecovered: string;
+  TodayDeath: string;
+  TotalCaseBefore: string;
+  source: string;
+  updateTime: string;
+  resultMessage: string;
+}
--- covid-web-next/pages/index.tsx
+++ covid-web-next/pages/index.tsx
@@ -2,40 +2,8 @@ import Card from "components/Card";
 import Title from "components/Title";
 import { getAllCondition } from "lib/apis";
 import { ECardTitles } from "lib/enum";
+import { GetAllCondition } from "lib/types";
 import cx from "styles/Main.module.scss";
-
-interface GetAllCondition {
-  resultCode: string;
-  TotalCase: string;
-  TotalRecovered: string;
-  TotalDeath: string;
-  NowCase: string;
-  city1n: string;
-  city2n: string;
-  city3n: string;
-  city4n: string;
-  city5n: string;
-  city1p: string;
-  city2p: string;
-  city3p: string;
-  city4p: string;
-  city5p: string;
-  recoveredPercentage: number;
-  deathPercentage: number;
-  checkingCounter: string;
-  checkingPercentage: string;
-  caseCount: string;
-  casePercentage: string;
-  notcaseCount: string;
-  notcasePercentage: string;
-  TotalChecking: string;
-  TodayRecovered: string;
-  TodayDeath: string;
-  TotalCaseBefore: string;
-  source: string;
-  updateTime: string;
-  resultMessage: string;
-}
 
 const Main = ({ data }: { data: GetAllCondition }) => {
   const {
